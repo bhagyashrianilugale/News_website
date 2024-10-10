a4d89b2536fdbcd73f3305ae9d524d911ede0695
bhagyashriugale <bhagyashriugale6970@gmail.com>
2024-10-10 08:45:40 +0530

    Update

--- src/Hooks/useCategory.js
+++ src/Hooks/useCategory.js
@@ -10,7 +10,7 @@ const useCategory = ( category )=>{
     const getNewsData = async()=>{
         try{
             // Data fetching from news api
-            const response = await fetch(`https://newsapi.org/v2/top-headlines?q=${ category }&apiKey=${ API_KEY }`);
+            const response = await fetch(`https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fnewsapi.org%2Fv2%2Ftop-headlines%3Fq%3D${ category }%26apiKey%3Df07c1f77e1ff4ffb827785f07e09c8c0`);
             const jsonData = await response?.json();
             console.log(jsonData);
            
